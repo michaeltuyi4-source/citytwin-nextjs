aae91b2f388bc3f383e79057f700815779a603f9
Michael <michaeltuyi4@gmail.com>
2026-08-03 16:53:54 -0400

    Add sitemap, fix robots disallows, align canonical signals to www

--- app/robots.ts
+++ app/robots.ts
@@ -3,11 +3,11 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: '*', allow: '/' },
-      { userAgent: 'GPTBot', allow: '/' },
-      { userAgent: 'ClaudeBot', allow: '/' },
-      { userAgent: 'PerplexityBot', allow: '/' },
+      { userAgent: '*', allow: '/', disallow: ['/results', '/auth/'] },
+      { userAgent: 'GPTBot', allow: '/', disallow: ['/results', '/auth/'] },
+      { userAgent: 'ClaudeBot', allow: '/', disallow: ['/results', '/auth/'] },
+      { userAgent: 'PerplexityBot', allow: '/', disallow: ['/results', '/auth/'] },
     ],
-    sitemap: 'https://citytwinapp.com/sitemap.xml',
+    sitemap: 'https://www.citytwinapp.com/sitemap.xml',
   }
 }
